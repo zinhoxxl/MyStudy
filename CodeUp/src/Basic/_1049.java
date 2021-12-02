93ba6ee1d1fba65849587e90e439cf6bab218f2f
alpha <zinhoxxl@gmail.com>
2021-12-02 22:39:51 +0900

    1202

---
+++ CodeUp/src/Basic/_1049.java
@@ -0,0 +1,16 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class _1049 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a<=b) {
+        	System.out.println(0);
+        }else {
+        	System.out.println(1);
+        }
+	}
+}
