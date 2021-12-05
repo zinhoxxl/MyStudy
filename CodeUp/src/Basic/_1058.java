ddd5708c48e00bb094704d581b9104ad14adb186
alpha <zinhoxxl@gmail.com>
2021-12-05 17:50:56 +0900

    1205

---
+++ CodeUp/src/Basic/_1058.java
@@ -0,0 +1,12 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class _1058 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a==0&b==0?1:0);
+	} 
+}
---
+++ CodeUp/src/Basic/_1060.java
@@ -0,0 +1,12 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class _1060 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.printf("%d", a&b);
+	} 
+}
