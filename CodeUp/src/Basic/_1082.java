5b6162b9443e81ddb27edaf9a407f49ea0582dbf
alpha <zinhoxxl@gmail.com>
2021-12-07 22:10:21 +0900

    1207

---
+++ CodeUp/src/Basic/_1082.java
@@ -0,0 +1,13 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class _1082 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt(16);
+		for(int i=1; i<16; i++) {
+			System.out.printf("%X*%X=%X\n", n, i, n*i);
+		}
+	}
+}
---
+++ CodeUp/src/Basic/_1083.java
@@ -0,0 +1,17 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class _1083 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i=1; i<=n; i++) {
+			if(i%3==0) {
+				System.out.print("X"+" ");
+			}
+			else
+				System.out.print(i + " ");
+		}
+	}
+}
